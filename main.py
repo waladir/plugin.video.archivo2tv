67bbbc9a3cd9e997315bdd070ed8d1397d506a59
waladir <waladir@hrbkovi.eu>
2020-05-11 07:04:58 +0200

    1.2.0

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import xbmcaddon
 import xbmc
 from urllib import urlencode, quote
 from urlparse import parse_qsl
-from urllib2 import urlopen, Request
+from urllib2 import urlopen, Request, HTTPError
+
 import json
 import uuid
 from datetime import datetime, timedelta 
@@ -33,7 +34,15 @@ def check_settings():
 def get_auth_token():
     post = {"username":addon.getSetting("username"),"password":addon.getSetting("password")}
     request = Request("https://www.o2tv.cz/unity/api/v1/services/", data = json.dumps(post), headers = headers)
-    html = urlopen(request).read()
+    try:
+      html = urlopen(request).read()
+    except HTTPError as e:
+      if e.reason == "Unauthorized":
+        xbmcgui.Dialog().notification("Archiv O2TV","Chybné přihlašovací údaje!", xbmcgui.NOTIFICATION_ERROR, 4000)
+        sys.exit()            
+      xbmcgui.Dialog().notification("Archiv O2TV","Problém s příhlášením", xbmcgui.NOTIFICATION_ERROR, 4000)
+      sys.exit()            
+
     data = json.loads(html)
     if "remoteAccessToken" in data and data["remoteAccessToken"] <>'' and "services" in data and len(data["services"]) > 0:
         remoteAccessToken = data["remoteAccessToken"]
@@ -64,6 +73,16 @@ def get_auth_token():
     else:
         xbmcgui.Dialog().notification("Archiv O2TV","Problém s příhlášením", xbmcgui.NOTIFICATION_ERROR, 4000)
         sys.exit()
+
+def list_menu():
+    list_item = xbmcgui.ListItem(label="Kanály")
+    url = get_url(action='list_channels')  
+    xbmcplugin.addDirectoryItem(_handle, url, list_item, True)
+    list_item = xbmcgui.ListItem(label="Vyhledávání")
+    url = get_url(action='program_search')  
+    xbmcplugin.addDirectoryItem(_handle, url, list_item, True)
+    xbmcplugin.endOfDirectory(_handle)
+
     
 def list_channels():
     request = Request("https://www.o2tv.cz/unity/api/v1/channels/", data = None, headers = headers)
@@ -104,13 +123,12 @@ def list_days(channelKey):
     xbmcplugin.endOfDirectory(_handle)
 
 def list_program(channelKey, day_min):
-    if day_min == 0:
+    if int(day_min) == 0:
       from_datetime = datetime.combine(date.today(), datetime.min.time())
       to_datetime = datetime.now()
     else:
       from_datetime = datetime.combine(date.today(), datetime.min.time()) - timedelta(days = int(day_min))
       to_datetime = datetime.combine(from_datetime, datetime.max.time())
-      
     from_ts = int(time.mktime(from_datetime.timetuple()))
     to_ts = int(time.mktime(to_datetime.timetuple()))
     request = Request("https://www.o2tv.cz/unity/api/v1/epg/depr/?channelKey=" + quote(channelKey) + "&from=" + str(from_ts*1000) + "&to=" + str(to_ts*1000) + "&forceLimit=true&limit=500", data = None, headers = headers)
@@ -121,29 +139,28 @@ def list_program(channelKey, day_min):
         start = datetime.fromtimestamp(programs["start"]/1000)
         end = datetime.fromtimestamp(programs["end"]/1000)
         epgId = programs["epgId"]
-        
-        request = Request("https://www.o2tv.cz/unity/api/v1/programs/" + str(epgId) + "/", data = None, headers = headers)
-        html = urlopen(request).read()   
-        data = json.loads(html)
-        img = "";
-        plot = "";
-        if "images" in data and len(data["images"]) > 0:
-           plot = data["longDescription"]
-           img = data["images"][0]["cover"]
- 
-        list_item = xbmcgui.ListItem(label= start.strftime("%d.%m %H:%M") + " - " + end.strftime("%H:%M") + " " + programs["name"])
-        list_item.setProperty("IsPlayable", "true")
-        list_item.setArt({'thumb': "https://www.o2tv.cz/" + img, 'icon': "https://www.o2tv.cz/" + img})
-        list_item.setInfo("video", {"mediatype":"movie", "title":programs["name"], "plot":plot})
+        if to_ts > int(programs["end"]/1000):        
+          request = Request("https://www.o2tv.cz/unity/api/v1/programs/" + str(epgId) + "/", data = None, headers = headers)
+          html = urlopen(request).read()   
+          data = json.loads(html)
+          img = "";
+          plot = "";
+          if "images" in data and len(data["images"]) > 0:
+             plot = data["longDescription"]
+             img = data["images"][0]["cover"]
+   
+          list_item = xbmcgui.ListItem(label= start.strftime("%d.%m %H:%M") + " - " + end.strftime("%H:%M") + " " + programs["name"])
+          list_item.setProperty("IsPlayable", "true")
+          list_item.setArt({'thumb': "https://www.o2tv.cz/" + img, 'icon': "https://www.o2tv.cz/" + img})
+          list_item.setInfo("video", {"mediatype":"movie", "title":programs["name"], "plot":plot})
 
-        if "ratings" in data and len(data["ratings"]) > 0:
-          for rating, value in data["ratings"].items():
-            if rating == "imdbRating":
-              list_item.setRating("imdb", float(value), 0, True)
-  
-        list_item.setContentLookup(False)          
-        url = get_url(action='play_video', epgId = epgId)
-        xbmcplugin.addDirectoryItem(_handle, url, list_item, False)
+          if "ratings" in data and len(data["ratings"]) > 0:
+            for rating, rating_value in data["ratings"].items():
+              list_item.setRating(rating, rating_value/10)
+    
+          list_item.setContentLookup(False)          
+          url = get_url(action='play_video', epgId = epgId)
+          xbmcplugin.addDirectoryItem(_handle, url, list_item, False)
     else:
         xbmcgui.Dialog().notification("Archiv O2TV","Problém s načtením programu", xbmcgui.NOTIFICATION_ERROR, 4000)
         sys.exit()    
@@ -169,12 +186,59 @@ def play_live(channelKey):
     data = json.loads(html)
     url = data["playlist"][0]["streamUrls"]["main"].replace("https://stc.o2tv.cz", "https://vst22-3.o2tv.cz:443")
     listitem = xbmcgui.ListItem(path = url)
-    print(url)
+    listitem.setInfo("video", {"mediatype":"movie", "title":data["setup"]["description"]})
     listitem.setProperty('inputstreamaddon', 'inputstream.adaptive')
     listitem.setProperty('inputstream.adaptive.manifest_type', 'mpd')
     listitem.setMimeType('application/dash+xml')
     listitem.setContentLookup(False)       
     xbmcplugin.setResolvedUrl(_handle, True, listitem)
+
+def program_search():
+    input = xbmc.Keyboard('', 'Hledat')
+    input.doModal()
+    if not input.isConfirmed(): 
+        return
+    query = input.getText()
+    if len(query) == 0:
+      xbmcgui.Dialog().notification("Archiv O2TV","Je potřeba zadat vyhledávaný řetězec", xbmcgui.NOTIFICATION_ERROR, 4000)
+      return   
+
+    max_ts = int(time.mktime(datetime.now().timetuple()))
+    request = Request("https://www.o2tv.cz/unity/api/v1/search/tv/depr/?groupLimit=1&maxEnd=" + str(max_ts*1000) + "&q=" + quote(query), data = None, headers = headers)
+    html = urlopen(request).read()   
+    data = json.loads(html)
+    
+    if "groupedSearch" in data and "groups" in data["groupedSearch"] and len(data["groupedSearch"]["groups"]) > 0:
+      for item in data["groupedSearch"]["groups"]:
+        programs = item["programs"][0]
+        start = datetime.fromtimestamp(programs["start"]/1000)
+        end = datetime.fromtimestamp(programs["end"]/1000)
+        epgId = programs["epgId"]
+        
+        request = Request("https://www.o2tv.cz/unity/api/v1/programs/" + str(epgId) + "/", data = None, headers = headers)
+        html = urlopen(request).read()   
+        data = json.loads(html)
+        img = "";
+        plot = "";
+        if "images" in data and len(data["images"]) > 0:
+           plot = data["longDescription"]
+           img = data["images"][0]["cover"]
+ 
+        list_item = xbmcgui.ListItem(label= start.strftime("%d.%m %H:%M") + " - " + end.strftime("%H:%M") + " " + programs["channelKey"] + " - " + programs["name"])
+        list_item.setProperty("IsPlayable", "true")
+        list_item.setArt({'thumb': "https://www.o2tv.cz/" + img, 'icon': "https://www.o2tv.cz/" + img})
+        list_item.setInfo("video", {"mediatype":"movie", "title":programs["name"], "plot":plot})
+
+        if "ratings" in data and len(data["ratings"]) > 0:
+          for rating, rating_value in data["ratings"].items():
+            list_item.setRating(rating, rating_value/10)
+  
+        list_item.setContentLookup(False)          
+        url = get_url(action='play_video', epgId = epgId)
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, False)
+      xbmcplugin.endOfDirectory(_handle)
+    else:
+      xbmcgui.Dialog().notification("Archiv O2TV","Nic nenalezeno", xbmcgui.NOTIFICATION_INFO, 3000)
 
 check_settings() 
 accessToken, sdata, channels = get_auth_token()
@@ -182,7 +246,9 @@ accessToken, sdata, channels = get_auth_token()
 def router(paramstring):
     params = dict(parse_qsl(paramstring))
     if params:
-        if params["action"] == "list_days":
+        if params["action"] == "list_channels":
+            list_channels()
+        elif params["action"] == "list_days":
             list_days(params["channelKey"])
         elif params['action'] == 'list_program':
             list_program(params["channelKey"], params["day_min"])
@@ -190,10 +256,12 @@ def router(paramstring):
             play_video(params["epgId"])
         elif params['action'] == 'play_live':
             play_live(params["channelKey"])
+        elif params['action'] == 'program_search':
+            program_search()
         else:
             raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
     else:
-        list_channels()
+        list_menu()
 
 
 if __name__ == '__main__':
